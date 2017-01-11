9854cd2c2a0b2bb862bf031622940212b76e45bf
sibizaestruch <sibizaestruch@gmail.com>
2017-01-11 20:58:35 +0100

    Add some GET methods for the API.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -18,7 +18,7 @@ class User
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\OneToMany(targetEntity="Message", mappedBy="device")
+     * @ORM\OneToMany(targetEntity="Message", mappedBy="user")
      */
     private $id;
 
